0f9bf0de96acbae19fa69fc75045837bb6d1dfc3
Joseph <josephylee72@gmail.com>
2023-11-20 21:49:48 -0800

    completed trains lab

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask import Flask, abort, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from werkzeug.exceptions import NotFound
+import ipdb
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -19,7 +20,7 @@ api = Api(app)
 class Trains(Resource):
     def get(self):
         try:
-            trains = [train.to_dict() for train in Train.query.all()]
+            trains = [train.to_dict() for train in Train.query]
             return make_response(trains, 200)
         except ValueError:
             return make_response({'errors' : 'could not fetch train data'}, 400)
@@ -30,14 +31,18 @@ class Trains(Resource):
             new_item = Train(**new_data)
             db.session.add(new_item)    
             db.session.commit()
-            return make_response(new_item.to_dict(), 200)
+            return make_response(new_item.to_dict(), 201)
         except ValueError:
             db.session.rollback()
             return make_response({'errors' : 'could not create new data'}, 400)
 
 class TrainsById(Resource):
     def get(self, id):
-        pass
+        try:
+            selected = db.session.get(Train, id)
+            return make_response(selected.to_dict(), 200)
+        except Exception:
+            return make_response({"error": "Train does not exist."}, 404)
 
 class Assignments(Resource):
     def get(self):
@@ -49,18 +54,23 @@ class Assignments(Resource):
         
     def post(self):
         try:
+            ipdb.set_trace()
             new_data = request.get_json()
             new_item = Assignment(**new_data)
             db.session.add(new_item)    
             db.session.commit()
             return make_response(new_item.to_dict(), 200)
-        except ValueError:
+        except Exception as e:
             db.session.rollback()
-            return make_response({'errors' : 'could not create new data'}, 400)
+            return make_response({'errors' : str(e)}, 400)
 
 class AssignmentsById(Resource):
     def get(self, id):
-        pass
+        try:
+            selected = db.session.get(Assignment, id)
+            return make_response(selected.to_dict(), 200)
+        except Exception:
+            return make_response({"error": "Assignment does not exist."}, 404)
 
 api.add_resource(Trains, '/trains')
 api.add_resource(TrainsById, '/trains/<int:id>')
